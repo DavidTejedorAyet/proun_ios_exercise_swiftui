8ed207ce781111a77f07c0e251941fe7685eff8d
David Tejedor <david.tejedor@outlook.com>
2021-09-01 18:11:14 +0200

    Initial Commit

---
+++ proun-ios-exercise-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  proun-ios-exercise-swiftui
+//
+//  Created by David on 1/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ proun-ios-exercise-swiftui/proun_ios_exercise_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  proun_ios_exercise_swiftuiApp.swift
+//  proun-ios-exercise-swiftui
+//
+//  Created by David on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct proun_ios_exercise_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
